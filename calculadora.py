175fbd04efc2625b515da2ddf5838982fb48903a
Saas Portifolio <Saasportifolio@gmail.com>
2026-02-27 10:24:13 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -2,13 +2,18 @@ from datetime import datetime, date
 
 class periodo:
   def __init__(self, inicio, fim):
-    dia_inicial = datetime.strptime(inicio,"%d/%m/%Y")
-    dia_final = datetime.strptime(fim,"%d/%m/%Y")
+    dia_inicial = datetime.strptime(inicio,"%Y-%m-%d")
+    dia_final = datetime.strptime(fim,"%Y-%m-%d")
     self.periodo_total = (dia_final - dia_inicial).days +1
 
 class calculadora:
-  def __init__(self, quantidade):
-    self.valor = 65
+  def __init__(self, quantidade,servicos):
+    precos = {
+    'hospedagem': 65,
+    'daycare': 35,
+    'passeio': 40
+    }
+    self.valor = precos.get(servicos, 65)
     self.quantidade = quantidade
 
   def calculo(self,obj_periodo):
